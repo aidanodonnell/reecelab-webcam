23ef24b92fcbfa9f3e87b754ae6245a937e21793
aidanodonnell <91739045+aidanodonnell@users.noreply.github.com>
2022-03-07 14:27:14 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "environment" }, audio: false };
+var constraints = { video: { facingMode: "user" }, audio: false };
 var track = null;
 
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor")
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
@@ -19,6 +20,16 @@ function cameraStart() {
             console.error("Oops. Something is broken.", error);
         });
 }
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+    // track.stop();
+};
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
