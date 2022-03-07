7d2539622dde80b44bb1f8e367b7462fad866b84
aidanodonnell <91739045+aidanodonnell@users.noreply.github.com>
2022-03-07 14:27:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var track = null;
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger");
+    cameraSensor = document.querySelector("#camera--sensor");
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
@@ -20,16 +19,6 @@ function cameraStart() {
             console.error("Oops. Something is broken.", error);
         });
 }
-
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-    // track.stop();
-};
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
